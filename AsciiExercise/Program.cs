16d968ffdad383aaaf3703537c933f12881078dc
Patrick Wegerer <patrick.wegerer@gmail.com>
2019-10-10 09:46:41 +0200

    Finished basic task

---
+++ AsciiExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using AsciiExerciseLibrary;
+namespace AsciiArtCombiner {
+    class Program {
+        static void Main(string[] args) {
+            var reader = new FileReader();
+            if(args.Length < 2) {
+                Console.WriteLine("Only one or less files specified");
+                Console.ReadLine();
+                Environment.Exit(5);
+            }
+            foreach (var path in args) {
+                try {
+                    reader.readFile(File.ReadAllLines(path));
+                } catch (Exception ex) {
+                    Console.WriteLine("File does not exist!");
+                    Console.ReadLine();
+                    Environment.Exit(5);
+                }
+                
+            }
+            //Console.WriteLine(Arrays.deepToString(reader.getResult()));
+            var result = reader.getResult();
+            for (int i = 0; i <= result.GetLength(0)-1; i++) {
+                for (int j = 0; j <= result.GetLength(1)-1; j++) {
+                    Console.Write(result[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ AsciiExerciseLibrary/FileReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace AsciiExerciseLibrary {
+    public class FileReader {
+        private string[,] result;
+        private int rows = 0;
+        private int columns = 0;
+        public FileReader() {
+
+        }
+
+        public void readFile(string[] lines) {
+            if (rows == 0) {
+                rows = lines.Length;
+                columns = lines[0].Length;
+                result = new string[rows,columns];
+            }
+            if(lines.Length != rows) {
+                Console.WriteLine("Size not Equal!"); //Insert Error Message
+                Console.ReadLine();
+                Environment.Exit(5);
+            }
+            for (int i=0; i < rows; i++) {
+                for(int j=0; j < columns; j++) {
+                    var curSpot = lines[i].ToCharArray()[j].ToString();
+                    if (lines[i].Length != columns) {
+                        Console.WriteLine("Size not Equal!");
+                        Console.ReadLine();
+                        Environment.Exit(5);
+                    }
+                    if(curSpot == " " && result[i,j] == null) {
+                        result[i, j] = curSpot;
+                    }
+                    if (curSpot != " " && result[i, j] == " " || result[i, j] == null) {
+                        result[i, j] = curSpot;
+                    }
+
+                }
+                    
+            }
+            
+        }
+        public string[,] getResult() {
+            return result;
+        }
+    }
+}
